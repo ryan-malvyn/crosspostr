1a028c0c4ade295f530d39d2cb9de738ae9f4d76
Ryan <ryanmalvyn2202@gmail.com>
2024-10-18 20:52:53 +0800

    ig integration

--- components/authentication/signIn/instagram.tsx
+++ components/authentication/signIn/instagram.tsx
@@ -4,12 +4,12 @@ export default function SignInWithInstagram(){
   const router = useRouter();
 
   function handleInstagramAuthorization(){
-    const clientId = '469165558845043'
+    const clientId = process.env.NEXT_PUBLIC_INSTAGRAM_CLIENT_ID
     const redirectURI = 'https://localhost:3000/api/instagramAuth/callback'
     const responseType='code'
     const scope = 'business_basic,business_content_publish,business_manage_comments,business_manage_messages'
    
-    const authorizationUrl = 'https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=469165558845043&redirect_uri=https://localhost:3000/api/instagramAuth/callback&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish'
+    const authorizationUrl = `https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=0&client_id=${clientId}&redirect_uri=${redirectURI}&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish`
     router.push(authorizationUrl)
   }
   return(
